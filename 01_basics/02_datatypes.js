46c6490730ee19b52d9a5ac4f3f59760fa8708e1
ShivamSingh321GO <getshivamsingh44@gmail.com>
2025-01-26 20:56:45 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+
+"use strict";//need to write this line so that js treats all code as newer version
+
+//alert(3+3)//we are using nodejs, not browser and alert only works on browser console
+
+//datatypes
+
+let name="hitesh";
+let age=18;
+let isLoggedIn=false;
+
+
+/*these are the datatpes in js
+
+number
+bigint
+string
+boolean
+null=>standalone value
+undefined
+symbol=>unique (used while working with react)
+
+object
+*/
+
+console.log(typeof(age));
+console.log(typeof(undefined));//output will be undfined
+console.log(typeof(null));//output will be object

92b0c04ed5c6627e5810e82900fcbf89be62c111
ShivamSingh321GO <getshivamsingh44@gmail.com>
2025-02-22 01:06:22 +0000

    .

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,7 +1,22 @@
 
 "use strict";//need to write this line so that js treats all code as newer version
 
-//alert(3+3)//we are using nodejs, not browser and alert only works on browser console
+/*
+  NOTE:
+   Using "use strict"; in JavaScript is important because it enables Strict Mode, which helps catch common coding mistakes
+   and enforces better coding practices. It prevents certain bad practices that could lead to unexpected behavior in JavaScript.
+
+    ex:
+
+    "use strict";
+    x = 10; //  ReferenceError: x is not defined (this error will be thrown when we run the program in strict mode)
+    console.log(x);
+
+ */
+
+x=10;
+console.log(10);
+//alert(3+3)//we are using nodejs, not browser therefore  alert only works on browser console
 
 //datatypes
 
@@ -12,6 +27,15 @@ let isLoggedIn=false;
 
 /*these are the datatpes in js
 
+NOTE: 
+
+In JavaScript, everything that is not a primitive type is an object. That includes arrays, errors, functions, dates, and
+ even wrapped versions of primitive types like strings and numbers.
+JavaScript treats many built-in structures as objects because they inherit properties and methods from prototypes.
+
+//TYPES of datatypes:
+
+1.primitive types
 number
 bigint
 string
@@ -20,7 +44,11 @@ null=>standalone value
 undefined
 symbol=>unique (used while working with react)
 
+2.non primitive types:
+
 object
+Arrays
+functions
 */
 
 console.log(typeof(age));

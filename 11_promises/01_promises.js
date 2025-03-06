5bb1d331c2d79ef2c7b2e99353db4279be8241d8
ShivamSingh321GO <getshivamsingh44@gmail.com>
2025-03-06 09:25:13 +0000

    ...

---
+++ 11_promises/01_promises.js
@@ -0,0 +1,45 @@
+
+//ex1: 
+
+// const promise1=new Promise(function(resolve, reject){
+
+//     setTimeout(function(){
+//         console.log('Async task is completed');
+//         resolve();//it is necessary to resolve otherwise the promise wont be consumed 
+        
+//     },1000);
+// })
+
+// promise1.then(function(){
+//     console.log('Promised is consumed');
+    
+// })
+
+//the example above can also be written as :
+
+// new Promise(function(resolve, reject){
+
+//     setTimeout(function(){
+//         console.log('Async task2 is completed');
+//         resolve();//it is necessary to resolve otherwise the promise wont be consumed 
+        
+//     },1000);
+// }).then(function(){
+//     console.log("Async task2 is resolved")
+// })
+
+//ex2: in this example we are going to print the resolved value of a promise.
+
+
+const promise2=new Promise(function(resolve, reject){
+
+    setTimeout(function(){
+         resolve({userName:"chai", emai:'chai@example.com'})
+    },1000);
+})
+
+promise2.then(function(user){
+    console.log(user);
+    
+})
+
